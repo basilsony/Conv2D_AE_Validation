891e3e10fbde5db83fc30bbb6662ac3ec8c6a543
basil-sony <basil.sony@hpe.com>
2020-10-27 17:40:53 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Activate environment and install packages
+conda init bash
+
+
+conda install -c conda-forge librosa
+
+conda install -c conda-forge tensorflow
+##conda install -c conda-forge tensorflow-gpu
+
+conda install -c conda-forge keras
+
+git clone https://github.com/tapojyotipaul/DenseAE
+
+git clone  https://github.com/basilsony/Conv2D_AE_Validation
+
+cd Conv2D_AE_Validation
+
+python3 conv2D_prediction.py
