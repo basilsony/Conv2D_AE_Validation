8bfb9997f88d5ea0e63c21472e7e7c97be27a67e
basil-sony <basil.sony@hpe.com>
2021-01-29 17:26:15 +0530

    Create run_op.sh

---
+++ run_op.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Activate environment and install packages
+conda init bash
+
+conda install -c conda-forge -y librosa
+
+conda install -c conda-forge -y tensorflow
+##conda install -c conda-forge tensorflow-gpu
+
+conda install -c conda-forge -y keras
+
+pip install openvino
+
+git clone https://github.com/tapojyotipaul/DenseAE
+git clone https://github.com/basilsony/Conv2D_AE_Validation
+cd Conv2D_AE_Validation
+
+python3 conv2D_prediction_openvino.py
